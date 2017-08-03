a5cd53536f7ae7ccfd9b7d6903b1891a4adfdc41
leonreteig@gmail.com <leonreteig@gmail.com>
2017-08-03 17:34:18 +0200

    Update avg MNI coordinate for full sample
    Also attached a notes file describing current state of the analysis,
    and how it should be improved

--- neuronav/mni_coords.sh
+++ neuronav/mni_coords.sh
@@ -17,18 +17,31 @@
 # S13: LN         LN.nii
 # S14: P06_sc1507 P06_sc1507_K1-20120515-0003-WIPsT13DTFE_P25_S2_3mSENSE.nii.gz
 # S15: MB         t1_1mm.nii.gz
-# S16: SOAT_21    SOAT_21-20161214-0003-WIPT1w_1mm_4m40_S1SENSE.nii
+# S16: SOAT_21    SOAT_21-20161214-0003-WIPT1w_1mm_4m40_S1SENSE.nii.gz
 # S17: SOAT_33    SOAT_33-20161230-0003-WIPT1w_1mm_4m40_S1SENSE.nii
 # S18: SOAT_11    SOAT_11-20161208-0003-WIPT1w_1mm_4m40_S1SENSE.nii
-# S19: SOAT_26    SOAT_26-20161215-0003-WIPT1w_1mm_4m40_S1SENSE.nii
+# S19: Unclear markers
 # S20: pp20_PH_1  PP20_PH_1correct-20170117-0006-WIPsT1W_3D_TFE_32channelSENSE.nii
-
-# S01 and S16 are to be excluded!
+# S21: sub-026    sub-026Active-CurioVal-20170421-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S22: sub-024    sub-024Active-CurioVal-20170421-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S23: experiment aborted
+# S24: sub-018    sub-018Active-CurioVal-20170411-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S25: sub-052    sub-052Passive-CurioVal-20170430-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S26: sub-003    sub-003Active-Curioval-20170407-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S27: sub-040    sub-040Passive-CurioVal-20170424-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S28: sub-051    sub-051Passive-CurioVal-20170425-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S29: BCS034     BCS034_UvA_T1.nii
+# S30: BCS038     BCS038_UvA_T1.nii
+# S31: did not show up
+# S32: sub-010    sub-010Active-Curioval-20170409-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
+# S33: sub-016    sub-016Active-CurioVal-20170411-0002-WIPT1anat_acq-3m_T1wSENSE.nii.gz
 
 homeDir=/Volumes/research$/reteig/sacc-tDCS/neuronav # working directory
 
 # list of folders in directory
-declare -a folders=("P03_sc1"
+declare -a folders=(
+"FFM_03"
+"P03_sc1"
 "DL"
 "MB_012"
 "BM"
@@ -41,17 +54,30 @@ declare -a folders=("P03_sc1"
 "LN"
 "P06_sc1507"
 "MB"
+"SOAT_21"
 "SOAT_33"
 "SOAT_11"
-"SOAT_26"
 "pp20_PH_1"
+"sub-026"
+"sub-024"
+"sub-018"
+"sub-052"
+"sub-003"
+"sub-040"
+"sub-051"
+"BCS034"
+"BCS038"
+"sub-010"
+"sub-016"
 )
 
 # get length the array
 numsubs=${#folders[@]}
 
 # list of scans (1 per folder)
-declare -a scans=("P03_sc1503_K1-20120514-0003-WIPsT13DTFE_P25_S2_3mSENSE"
+declare -a scans=(
+"FFM_03-20150504-0005-WIPsT13DTFE_P25_S2_3mSENSE.nii.gz"
+"P03_sc1503_K1-20120514-0003-WIPsT13DTFE_P25_S2_3mSENSE"
 "DL_140514_10"
 "MB_012-20141118-0011-WIPsT13DTFE3mSENSE"
 "BM_T1_reorient"
@@ -64,10 +90,21 @@ declare -a scans=("P03_sc1503_K1-20120514-0003-WIPsT13DTFE_P25_S2_3mSENSE"
 "LN"
 "P06_sc1507_K1-20120515-0003-WIPsT13DTFE_P25_S2_3mSENSE"
 "t1_1mm"
+"SOAT_21-20161214-0003-WIPT1w_1mm_4m40_S1SENSE"
 "SOAT_33-20161230-0003-WIPT1w_1mm_4m40_S1SENSE"
 "SOAT_11-20161208-0003-WIPT1w_1mm_4m40_S1SENSE"
-"SOAT_26-20161215-0003-WIPT1w_1mm_4m40_S1SENSE"
 "PP20_PH_1correct-20170117-0006-WIPsT1W_3D_TFE_32channelSENSE"
+"sub-026Active-CurioVal-20170421-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-024Active-CurioVal-20170421-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-018Active-CurioVal-20170411-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-052Passive-CurioVal-20170430-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-003Active-Curioval-20170407-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-040Passive-CurioVal-20170424-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-051Passive-CurioVal-20170425-0002-WIPT1anat_acq-3m_T1wSENSE"
+"BCS034_UvA_T1"
+"BCS038_UvA_T1"
+"sub-010Active-Curioval-20170409-0002-WIPT1anat_acq-3m_T1wSENSE"
+"sub-016Active-CurioVal-20170411-0002-WIPT1anat_acq-3m_T1wSENSE"
 )
 
 for (( i=0; i<${numsubs}; i++ )); # loop over subjects
