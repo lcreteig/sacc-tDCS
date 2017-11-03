eb4959b0eb3b35e0964ba8cc992f57e49c522af4
leonreteig@gmail.com <leonreteig@gmail.com>
2017-11-03 12:07:27 +0100

    Add option to rerun without BET and FLIRT

--- src/func/mni_coords.sh
+++ src/func/mni_coords.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # First argument: input csv file with native coordinates
 # Second argument: output csv file with MNI coordinates
-#
+# Third argument (optional): "all" to also (redo) strip skull (BET) and register to standard (FLIRT)
 # e.g.
-# bash mni_coords.sh FEF_coords_native.csv FEF_coords_MNI.csv
+# bash mni_coords.sh FEF_coords_native.csv FEF_coords_MNI.csv all
 
 inFile=$1
 outFile=$2
+bet_flirt=$3
+
 echo "subject;folder;scan;MNI_X;MNI_Y;MNI_Z" > $outFile # header for new file
 
 # read all columns of file, line by line
@@ -19,11 +21,14 @@ do
 
     mkdir -p "$writeFolder" # if it doesn't exist yet, create directory
 
-    # BET (strip the skull)
-    bet ${readFolder}/${scan}.nii ${writeFolder}/${scan}_brain.nii.gz
+    if [ $bet_flirt = "all" ]; then
 
-    # FLIRT (register to MNI)
-    flirt -in ${writeFolder}/${scan}_brain.nii.gz -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain -out ${writeFolder}/${scan}_brain_toMNI1mm.nii.gz -omat ${writeFolder}/${folder}_native2mNI.mat
+      # BET (strip the skull)
+      bet ${readFolder}/${scan}.nii ${writeFolder}/${scan}_brain.nii.gz
+
+      # FLIRT (register to MNI)
+      flirt -in ${writeFolder}/${scan}_brain.nii.gz -ref ${FSLDIR}/data/standard/MNI152_T1_1mm_brain -out ${writeFolder}/${scan}_brain_toMNI1mm.nii.gz -omat ${writeFolder}/${folder}_native2mNI.mat
+    fi
 
     # Convert coordinates to MNI
     MNI_mm=$(echo "$native_X $native_Y $native_Z" | img2stdcoord -img ${readFolder}/${scan} -std ${FSLDIR}/data/standard/MNI152_T1_1mm -xfm ${writeFolder}/${folder}_native2mNI.mat)
